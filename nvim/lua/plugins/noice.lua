acc03ad457c9ddc22e678e0365f1070ca12317f6
thom <thomblweed@gmail.com>
2024-06-08 20:51:41 +0100

    saving latest

---
+++ nvim/lua/plugins/noice.lua
@@ -0,0 +1,14 @@
+return {
+  "folke/noice.nvim",
+  opts = {
+    lsp = {
+      signature = {
+        auto_open = {
+          -- remove ts hints in the FOV
+          -- enabled = false,
+          -- hints = false,
+        },
+      },
+    },
+  },
+}
